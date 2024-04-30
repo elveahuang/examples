1f270a5c5bdc80675b20f7e231c8ee0f89333dcf
Elvea Huang <elvea.huang@gmail.com>
2024-04-30 23:10:43 +0800

    初始ShardingSphere示例

--- spring-boot-examples/spring-boot-shardingsphere-example/src/main/java/cn/elvea/boot/shardingsphere/Application.java
+++ spring-boot-examples/spring-boot-shardingsphere-example/src/main/java/cn/elvea/boot/shardingsphere/Application.java
@@ -3,9 +3,10 @@ package cn.elvea.boot.shardingsphere;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan("cn.elvea.boot.shardingsphere.mapper")
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
