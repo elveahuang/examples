9fe3b14ec0a565c95d417f10f8d302ac3dbe8fa0
Elvea Huang <elvea.huang@gmail.com>
2025-05-20 11:08:40 +0800

    Spring AI 1.0.0

--- spring-boot-examples/spring-boot-web-example/gulpfile.mjs
+++ spring-boot-examples/spring-boot-web-example/gulpfile.mjs
@@ -1,35 +1,39 @@
 import {dest, src} from "gulp";
 
-const nodeModulesDir = 'node_modules';
-const targetDir = 'src/main/webapp/static/libs';
+const nodeModulesDir = "node_modules";
+const targetDir = "src/main/webapp/static/libs";
 
 const copyLibs = async () => {
     // jQuery
-    src(nodeModulesDir + '/jquery/dist/jquery.js')
-        .pipe(dest(targetDir + '/jquery'));
+    src(nodeModulesDir + "/jquery/dist/jquery.js").pipe(
+        dest(targetDir + "/jquery"),
+    );
 
-    src(nodeModulesDir + '/jquery/dist/jquery.min.js')
-        .pipe(dest(targetDir + '/jquery'));
+    src(nodeModulesDir + "/jquery/dist/jquery.min.js").pipe(
+        dest(targetDir + "/jquery"),
+    );
 
     // popperjs
-    src(nodeModulesDir + '/@popperjs/core/dist/umd/popper.js')
-        .pipe(dest(targetDir + '/popperjs'));
+    src(nodeModulesDir + "/@popperjs/core/dist/umd/popper.js").pipe(
+        dest(targetDir + "/popperjs"),
+    );
 
-    src(nodeModulesDir + '/@popperjs/core/dist/umd/popper.min.js')
-        .pipe(dest(targetDir + '/popperjs'));
+    src(nodeModulesDir + "/@popperjs/core/dist/umd/popper.min.js").pipe(
+        dest(targetDir + "/popperjs"),
+    );
 
     // Bootstrap
-    src(nodeModulesDir + '/bootstrap/dist/**')
-        .pipe(dest(targetDir + '/bootstrap'));
+    src(nodeModulesDir + "/bootstrap/dist/**").pipe(
+        dest(targetDir + "/bootstrap"),
+    );
 
     // Layui
-    src(nodeModulesDir + '/layui/dist/**')
-        .pipe(dest(targetDir + '/layui'));
-}
+    src(nodeModulesDir + "/layui/dist/**").pipe(dest(targetDir + "/layui"));
+};
 
 const defaultTask = async () => {
     console.log("Copy libs.");
     await copyLibs().then();
-}
+};
 
 export default defaultTask;
