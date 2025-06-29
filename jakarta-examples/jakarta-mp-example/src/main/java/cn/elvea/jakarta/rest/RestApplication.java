ce709b918ec8a40785f0258fdc4d4739bf9d2417
Elvea Huang <elvea.huang@gmail.com>
2025-06-29 21:41:30 +0800

    microprofile 7.1 && spring boot 3.5.3

---
+++ jakarta-examples/jakarta-mp-example/src/main/java/cn/elvea/jakarta/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package cn.elvea.jakarta.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
