a4ed85fe3d13cb7646cfeb58ca2c17c72324569d
Elvea Huang <elvea.huang@gmail.com>
2026-03-05 18:06:44 +0800

    项目整理，只保留Spring Boot相关应用

--- spring-boot-examples/spring-boot-web-example/src/main/java/cc/elvea/boot/web/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package cc.elvea.boot.web.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
-
-@Configuration(proxyBeanMethods = false)
-@EnableRedisIndexedHttpSession(maxInactiveIntervalInSeconds = 3600)
-public class AppConfig {
-}
--- spring-boot-examples/spring-boot-web-example/src/main/java/cc/elvea/boot/web/filter/CustomSiteMeshFilter.java
+++
@@ -1,6 +0,0 @@
-package cc.elvea.boot.web.filter;
-
-import org.sitemesh.config.ConfigurableSiteMeshFilter;
-
-public class CustomSiteMeshFilter extends ConfigurableSiteMeshFilter {
-}
